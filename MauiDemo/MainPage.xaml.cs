3d0434297876ba87bbb77dc989374e27e39e5b27
none <none>
2023-10-18 09:58:11 +0200

    Add project files.

---
+++ MauiDemo/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace MauiDemo
+{
+    public partial class MainPage : ContentPage
+    {
+        private int _value = 100;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            lblCounter.Text = _value.ToString();
+        }
+
+        private void Num_Clicked(object sender, EventArgs e)
+        {
+            _value += Convert.ToInt32((sender as Button).Text);
+            lblCounter.Text = _value.ToString();
+        }
+
+    }
+}
